6ff81d1fd27233626a69dfd5e9bc1de7f38df7a0
Richard Newell <richardnewell15@gmail.com>
2024-09-10 14:25:49 -0700

    update

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -10,3 +10,62 @@ if (userRole === "admin") {
 }
 
 console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
+
+let userJobRole = "Employee";
+let userDietary;
+
+switch (userJobRole) {
+    case "Employee":
+        userDietary = "Dietary Services";
+        break;
+    case "Enrolled Member":
+        userDietary = "Dietary Services and one-on-one interaction with a dietician";
+        break;
+    case "Subscriber":
+        userDietary = "partial access to facilitate Dietary Services only.";
+        break;
+    default:
+        userDietary = "enroll or subscribe first to avail this facility";
+}
+
+console.log("User Dietary Access:", userDietary);
