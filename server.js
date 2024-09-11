74be0ace9c31c6d614bedf163901138ba5a3e146
Richard Newell <richardnewell15@gmail.com>
2024-09-11 15:52:27 -0700

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+var axios = require('axios');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+server.listen(8000);
